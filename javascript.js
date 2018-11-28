9edb189b146a89885935e4299b59dbab31ba6b7d
jakubpaszkowski <jakub.paszkowski@pearson.com>
2018-11-28 16:15:04 +0100

    set and weakset

--- javascript.js
+++ javascript.js
@@ -1,25 +1,52 @@
 logo = obj => console.log('drukowany obiekt: \n', JSON.stringify(obj, null, "  "));
+hr = () => console.log('--------------------------------');
 
 
 
-let map = new Map(),
-weakMap = new WeakMap();
+let set = new Set();
+
+set.add('tak');
+
+set.add(1);
+
+set.add('nie');
+
+set.add('tak');
+
+console.log(set.size);
+
+console.log(set.keys());
+
+console.log(set.values());
+
+console.log(set.has(2));
+
+
+let array = [1,2,3,4,3,2,5];
+
+let unique = Array.from(new Set(array));
+
+console.log('unique', unique);
+
+hr();
+
+let weakSet = new WeakSet();
 
 let obj1 = {
-	foo: 'foo'
+	a: 'pierwszy'
 },
 obj2 = {
-	bar: 'bar'
+	b: 'drugi'
 }
 
-map.set(obj1, 'pierwszy');
-weakMap.set(obj2, 'drugi');
+set.add(obj1);
 
-console.log(map.get(obj1));
-console.log(weakMap.get(obj2));
+weakSet.add(obj1);
 
-obj1 = undefined;
-obj2 = undefined;
+console.log(set.has(obj1));
+console.log(weakSet.has(obj1));
 
-console.log(map.get(obj1));
-console.log(weakMap.get(obj2));
+obj1 = null;
+
+console.log(set.has(obj1));
+console.log(weakSet.has(obj1));
